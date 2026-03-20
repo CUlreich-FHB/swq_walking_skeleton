7541351d304d97d537072bb9cf0396a52594ce68
CUlreich-FHB <2410859028@hochschule-burgenland.at>
2026-03-20 17:08:43 +0100

    feat: Added Spotless Plugin

--- src/main/java/at/hochschule/burgenland/walking_skeleton/WalkingSkeletonApplication.java
+++ src/main/java/at/hochschule/burgenland/walking_skeleton/WalkingSkeletonApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WalkingSkeletonApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(WalkingSkeletonApplication.class, args);
-    }
-
+  public static void main(String[] args) {
+    SpringApplication.run(WalkingSkeletonApplication.class, args);
+  }
 }
